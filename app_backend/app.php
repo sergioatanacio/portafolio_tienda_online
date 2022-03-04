a766d483e1d3e9b95b1047915b6bc88eaca26ecf
sergio14camino <sergio14camino@gmail.com>
2022-03-04 09:24:13 -0500

    Añadiendo interfaz

--- app_backend/app.php
+++ app_backend/app.php
@@ -1,5 +1,61 @@
 <?php
 
 $response = [
-    '/inicio' => 'Hola mundo',
+    '/productos' => '{
+        [
+            "id":       "1",
+            "nombre":   "ibuprofeno",
+            "precio":   "5.5",
+            "imagen":   "/assets/ibuprofeno.jpg",
+        ],
+        [
+            "id":       "2",
+            "nombre":   "parecetamol",
+            "precio":   "6.5",
+            "imagen":   "/assets/parasetamol.jpg",
+        ],
+        [
+            "id":       "3",
+            "nombre":   "parecetamol",
+            "precio":   "6.5",
+            "imagen":   "/assets/parasetamol.jpg",
+        ],  
+        [
+            "id":       "4",
+            "nombre":   "parecetamol",
+            "precio":   "6.5",
+            "imagen":   "/assets/parasetamol.jpg",
+        ],  
+        [
+            "id":       "5",
+            "nombre":   "parecetamol",
+            "precio":   "6.5",
+            "imagen":   "/assets/parasetamol.jpg",
+        ],  
+        [
+            "id":       "6",
+            "nombre":   "parecetamol",
+            "precio":   "6.5",
+            "imagen":   "/assets/parasetamol.jpg",
+        ],  
+    }',
+    '/carrito' => json_encode([
+        [
+            "id"    =>  "1",
+            "nombre"=>   "parecetamol",
+            "precio"=>   "6.5",
+            "imagen"=>   "/assets/parasetamol.jpg",
+        ],
+        [
+            "id"    =>  "2",
+            "nombre"=>   "parecetamol",
+            "precio"=>   "6.5",
+            "imagen"=>   "/assets/parasetamol.jpg",
+        ],  
+    ]),
 ];
+
+
+
+
+
